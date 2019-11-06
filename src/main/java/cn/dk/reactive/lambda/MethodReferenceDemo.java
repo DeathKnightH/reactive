bc3a275bbc67a3a9ed745677a8622ba5d84f4bdd
DeathKnightH <919438785@qq>
2019-11-06 18:22:38 +0800

    使用方法引用作为lambda表达式的样例研究

---
+++ src/main/java/cn/dk/reactive/lambda/MethodReferenceDemo.java
@@ -0,0 +1,13 @@
+package cn.dk.reactive.lambda;
+
+import java.util.function.Consumer;
+
+public class MethodReferenceDemo {
+    public static void main(String[] args) {
+        // 方法引用
+        Consumer<String> consumer = System.out::println;
+
+        // 静态方法引用
+
+    }
+}
